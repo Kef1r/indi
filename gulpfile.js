31c837a38be96b8b556d9865c82580a2109a9863
kef1r <ikef1rka@gmail.com>
2017-04-13 16:14:50 +0300

    indi prod

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+
+var path = {html:['index.html']};
+
+
+ 
+
+gulp.task('sass', function(){
+	return gulp.src('./sass/*.scss')
+	.pipe(sass() .on('error', sass.logError))
+	.pipe(gulp.dest("css"))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('html', function(){
+	gulp.src(path.html)
+	.pipe(reload({stream:true}));
+});
+
+
+gulp.task('watch',['sass'], function(){
+	browserSync.init({
+		server: "./"
+	});
+  gulp.watch('./sass/*.scss',['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  
+
+});
+
+gulp.task('default', ['watch']);
